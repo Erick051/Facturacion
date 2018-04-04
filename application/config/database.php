fddc72b06a29aec473d55bb6b2f1aa9c64bcc31d
STOFACTURA\ermartinez <erick_martinez@stoconsulting.com>
2018-04-04 12:34:04 -0500

    [MOD] Se modifica el accesso a BD de local a servidor de pruebas

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => '172.20.5.253',
+	'username' => 'apps',
+	'password' => 'apps',
 	'database' => 'decathlon_neon',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
